b25fd679d248688c2631b5c9917d2456e796d4c1
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-09 02:02:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,7 +18,30 @@ def using_shift(array)
   array.shift
 end
 
-def
+def shift_with_args(array)
+  array.shift(2)
+end 
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array[4].insert(element)
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
 
 
